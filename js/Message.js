b9c77ece2065da7dfdb8d9ec92cbee42c4577bb3
bofeiw <bofeiw@icloud.com>
2019-04-14 01:05:34 +1000

    implement toHTML function

--- js/Message.js
+++ js/Message.js
@@ -2,4 +2,14 @@
 
 function Message(content) {
     this.content = content;
+
+    this.toHTML = function () {
+        const boxWrapper = $("<div>").addClass("box-wrapper");
+        const box = $("<div>").addClass("message-box");
+        const text = $("<div>").addClass("message-text");
+        box.append(text);
+        boxWrapper.append(box);
+        text.append(content);
+        return boxWrapper;
+    }
 }
