2c2e3fab5ec338f12d970e93d6d1e4336df3926a
bofeiw <bofeiw@icloud.com>
2019-04-14 15:19:47 +1000

    add id manager

--- js/message.js
+++ js/message.js
@@ -1,38 +1,68 @@
 "use strict";
 
-function Message(content) {
-    this.toHTML = function () {
+const Message = (function () {
+    let messageID = 0;
+
+    function nextID() {
+        return messageID++;
+    }
+
+    return function (content) {
+        const ID = nextID();
+        const messageContainer = $("#message-container");
         const boxWrapper = $("<div>").addClass("box-wrapper");
         const box = $("<div>").addClass("message-box");
         const text = $("<div>").addClass("message-text");
+
         box.append(text);
         boxWrapper.append(box);
         text.append(content);
-        return boxWrapper;
-    };
 
+        box.attr("id", boxID());
+        boxWrapper.attr("id", boxWrapperID());
+        text.attr("id", textID());
 
-    this.content = content;
-    const that = this;
-    this.HTML = this.toHTML();
-
-    return {
-        get content() {
-            return that.content;
-        },
-
-        get HTML() {
-            return that.HTML;
+        function boxID() {
+            return "box-" + ID;
         }
 
-    };
-}
+        function boxWrapperID() {
+            return "boxWrapper-" + ID;
+        }
+
+        function textID() {
+            return "text-" + ID;
+        }
+
+        function appendToContainer() {
+            messageContainer.append(boxWrapper);
+        }
+
+        /* make message object appears */
+        function appear() {
+            // boxWrapper.style.height = boxWrapper.clientHeight + "px";
+
+            // boxWrapper.addClass("appeared")
+        }
+
+        return {
+            get content() {
+                return content;
+            },
+
+            appendToContainer: appendToContainer,
+            appear: appear
+        }
+    }
+})();
 
 
 function showMessages(jsonData) {
     for (const message of jsonData.messages) {
         console.log(message);
-        $("#message-container").append(new Message(message.content).HTML);
+        const messageObject = new Message(message.content);
+        messageObject.appendToContainer();
+        messageObject.appear();
     }
 }
 
